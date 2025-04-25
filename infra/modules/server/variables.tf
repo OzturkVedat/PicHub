9bd73a89cd728f65b83df90307bffab448fa97d5
Vedat Ozturk <ozturkvedat72@gmail.com>
2025-04-25 14:24:10 +0300

    minor fix

--- infra/modules/server/variables.tf
+++ infra/modules/server/variables.tf
@@ -5,9 +5,9 @@ variable "public_subnet_id" {}
 variable "api_image_uri" {}
 
 variable "param_resource" {
-  default = "arn:aws:ssm:eu-north-1:962546904675:parameter/pichub"
+  default = "arn:aws:ssm:eu-north-1:<account-id>:parameter/pichub"
 }
 
 variable "user_pool_arn" {
-  default = "arn:aws:cognito-idp:eu-north-1:962546904675:userpool/eu-north-1_oHWl79A66"
+  default = "arn:aws:cognito-idp:eu-north-1:<account-id>:userpool/eu-north-1_xxxxxxx"
 }
