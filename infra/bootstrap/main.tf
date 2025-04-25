d16a5b5b724a2f0aa327a810ae0d5082ef59d9e7
Vedat Ozturk <ozturkvedat72@gmail.com>
2025-04-25 14:50:32 +0300

    minor addition

--- infra/bootstrap/main.tf
+++ infra/bootstrap/main.tf
@@ -1,3 +1,17 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+}
+
+provider "aws" {
+  profile = "default"
+  region  = "eu-north-1"
+}
+
 resource "aws_ssm_parameter" "param_resource" {
   name  = "/ivote/param_resource"
   type  = "SecureString"
